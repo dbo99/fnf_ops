524edb624c933e98fa262e8a2194e50f8b870e84
dbo99 <oconnor.david.b@gmail.com>
2019-09-02 15:35:53 -0400

    Add files via upload

--- ops_fnf_control.r
+++ ops_fnf_control.r
@@ -1,13 +1,18 @@
 
+
+
+rm(list = ls())
+
 {
+ 
+keepers <- c("mill_fnf", "trin_fnf", "whis_fnf", "shst_fnf", 
+               "pnft_fnf", "moke_fnf", "orov_fnf", "keepers", "daysback", "df")
 rm(list=setdiff(ls(), keepers))
 rstudioapi::getActiveDocumentContext
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-keepers <- c("mill_fnf", "trin_fnf", "whis_fnf", "shst_fnf", 
-             "pnft_fnf", "moke_fnf", "keepers", "daysback")
-
-daysback <- 12
+daysback <- 14
+}
 
 {
 source("opsfnf_cegc1.r")
@@ -16,14 +21,80 @@ source("opsfnf_shdc1.r")
 source("opsfnf_frac1.r")
 source("opsfnf_pftc1.r")
 source("opsfnf_cmpc1.r")
+#source("opsfnf_ordc1.r")
+}
 
+#### oroville special case (their last month report not always available!)
+
+
+c_monyr <- read_html('http://cdec.water.ca.gov/cgi-progs/reports.cur?s=fnf') %>%
+  html_nodes("h1") %>% html_text()
+c_monyr <- gsub("Daily Full Natural Flows for ", "", c_monyr)
+c_monyr <- as.yearmon(c_monyr)
+c_mon <- month(c_monyr)
+c_yr <- year(c_monyr)
+c_yr
+
+
+## java/css page
+
+# current month fnf
+c_fnf <-  readHTMLTable("http://cdec.water.ca.gov/reportapp/javareports?name=FNF", as.data.frame = TRUE)
+
+c_fnf_toptable <- c_fnf[[1]]
+#c_fnf_bottable <- c_fnf[[2]]  #only need top table for oroville
+
+c_ordc1_fnf <- c_fnf_toptable %>% transmute(V1, V5)
+c_ordc1_fnf <- c_ordc1_fnf[-1,] %>% transmute(day = V1, c_ordc1_fnf = V5)
+c_ordc1_fnf <- c_ordc1_fnf %>% mutate(day = as.numeric(as.character(day)))
+c_ordc1_fnf <- na.omit(c_ordc1_fnf)
+c_ordc1_fnf$c_ordc1_fnf <- gsub(",", "", c_ordc1_fnf$c_ordc1_fnf)
+c_ordc1_fnf <- c_ordc1_fnf %>% transmute(day, ordc1_fnf = as.numeric(as.character(c_ordc1_fnf)))
+c_ordc1_fnf <- c_ordc1_fnf %>% transmute(chps_date = paste0(c_yr,"-", c_mon, "-", day), 
+                                         chps_date = ymd(chps_date) + 1, ordc1_fnf)
+
+c_ordc1_fnf
+
+
+# previous month
+
+# previous published month
+p_monyr <- as.yearmon(c_monyr) - 1/12
+p_mon <- month(p_monyr)
+p_mon
+p_yr <- year(p_monyr)
+p_yr
+p_monyr <- p_monyr %>% format("%Y%m")
+p_monyr
+p_month_url <- paste0("http://cdec.water.ca.gov/reportapp/javareports?name=FNF.", p_monyr)
+#p_month_url <- "http://cdec.water.ca.gov/reportapp/javareports?name=FNF.201906" for testing when prev month not published online
+p_month_url
+
+p_fnf <-  readHTMLTable(p_month_url, as.data.frame = TRUE)
+
+p_fnf_toptable <- p_fnf[[1]]
+#p_fnf_bottable <- p_fnf[[2]]  #only need top table for oroville
+
+p_ordc1_fnf <- p_fnf_toptable %>% transmute(V1, V5)
+p_ordc1_fnf <- p_ordc1_fnf[-1,] %>% transmute(day = V1, p_ordc1_fnf = V5)
+p_ordc1_fnf <- p_ordc1_fnf %>% mutate(day = as.numeric(as.character(day)))
+p_ordc1_fnf <- na.omit(p_ordc1_fnf)
+p_ordc1_fnf$p_ordc1_fnf <- gsub(",", "", p_ordc1_fnf$p_ordc1_fnf)
+p_ordc1_fnf <- p_ordc1_fnf %>% transmute(day, ordc1_fnf = as.numeric(as.character(p_ordc1_fnf)))
+p_ordc1_fnf <- p_ordc1_fnf %>% transmute(chps_date = paste0(p_yr,"-", p_mon, "-", day), 
+                                         chps_date = ymd(chps_date) + 1, ordc1_fnf)
+p_ordc1_fnf
+
+
+
+orov_fnf <- rbind(c_ordc1_fnf, if(exists("p_ordc1_fnf")) p_ordc1_fnf)
+orov_fnf
 
 today <- Sys.Date()
-
 chps_date <- seq(today, by = "-1 day", length.out = daysback) 
 chps_date <- data.frame(chps_date)
 chps_date
-}
+
 
 df <- left_join(chps_date, trin_fnf)
 df <- left_join(df,  whis_fnf)
@@ -31,9 +102,10 @@ df <- left_join(df,  shst_fnf)
 df <- left_join(df,  mill_fnf)
 df <- left_join(df,  pnft_fnf)
 df <- left_join(df,  moke_fnf)
-}
+df <- left_join(df,  orov_fnf)
+df
 df <- df %>% transmute(chps_date, cegc1 = trin_fnf, whsc1 = whi_fnf, shdc1 = shst_fnf,
-                       frac1 = mill_fnf, pftc1 = pnft_fnf, cmpc1 = moke_fnf)
+                       frac1 = mill_fnf, pftc1 = pnft_fnf, cmpc1 = moke_fnf, ordc1 = ordc1_fnf)
 df
 df <- df %>% mutate_if(is.factor, as.character)
 df$cegc1 <- gsub(",", "", df$cegc1)
@@ -42,9 +114,11 @@ df$frac1 <- gsub(",", "", df$frac1)
 df$pftc1 <- gsub(",", "", df$pftc1)
 df$cmpc1 <- gsub(",", "", df$cmpc1)
 df$whsc1 <- gsub(",", "", df$whsc1)
+df$ordc1 <- gsub(",", "", df$ordc1)
 
 df
 df <- df %>% mutate_if(is.character, as.numeric)
-df <- df %>% transmute(chps_date, cegc1, whsc1, shdc1, cmpc1, frac1, pftc1)
-df
+df <- df %>% transmute(chps_date, cegc1, whsc1, shdc1, cmpc1, frac1, pftc1, ordc1)
 
+df
+rm(list=setdiff(ls(), keepers))
--- opsfnf_ordc1.r
+++ opsfnf_ordc1.r
@@ -1,34 +1,77 @@
+rm(list=setdiff(ls(), keepers))
+
+
+rstudioapi::getActiveDocumentContext
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+
+
 ## old page
 
 
-c_fnf1 <-  read_html("http://cdec.water.ca.gov/cgi-progs/reports.cur?s=fnf")
-c_fnf1[3]
+c_monyr <- read_html('http://cdec.water.ca.gov/cgi-progs/reports.cur?s=fnf') %>%
+  html_nodes("h1") %>% html_text()
+c_monyr <- gsub("Daily Full Natural Flows for ", "", c_monyr)
+c_monyr <- as.yearmon(c_monyr)
+c_mon <- month(c_monyr)
+c_yr <- year(c_monyr)
+c_yr
+
 
 ## java/css page
 
 # current month fnf
-c_fnf2 <-  readHTMLTable("http://cdec.water.ca.gov/reportapp/javareports?name=FNF", as.data.frame = TRUE)
+c_fnf <-  readHTMLTable("http://cdec.water.ca.gov/reportapp/javareports?name=FNF", as.data.frame = TRUE)
 
-c_fnf_toptable <- c_fnf2[[1]]
+c_fnf_toptable <- c_fnf[[1]]
 #c_fnf_bottable <- c_fnf[[2]]  #only need top table for oroville
 
-ordc1_fnf <- c_fnf_toptable %>% transmute(V1, V5)
-ordc1_fnf <- ordc1_fnf[-1,] %>% transmute(day = V1, ordc1_fnf = V5)
-ordc1_fnf <- ordc1_fnf %>% mutate(day = as.numeric(as.character(day)))
-ordc1_fnf <- na.omit(ordc1_fnf)
-ordc1_fnf$ordc1_fnf <- gsub(",", "", ordc1_fnf$ordc1_fnf)
-ordc1_fnf <- ordc1_fnf %>% mutate(ordc1_fnf = as.numeric(as.character(ordc1_fnf)))
-ordc1_fnf <
+c_ordc1_fnf <- c_fnf_toptable %>% transmute(V1, V5)
+c_ordc1_fnf <- c_ordc1_fnf[-1,] %>% transmute(day = V1, c_ordc1_fnf = V5)
+c_ordc1_fnf <- c_ordc1_fnf %>% mutate(day = as.numeric(as.character(day)))
+c_ordc1_fnf <- na.omit(c_ordc1_fnf)
+c_ordc1_fnf$c_ordc1_fnf <- gsub(",", "", c_ordc1_fnf$c_ordc1_fnf)
+c_ordc1_fnf <- c_ordc1_fnf %>% transmute(day, ordc1_fnf = as.numeric(as.character(c_ordc1_fnf)))
+c_ordc1_fnf <- c_ordc1_fnf %>% transmute(chps_date = paste0(c_yr,"-", c_mon, "-", day), 
+                                         chps_date = ymd(chps_date) + 1, ordc1_fnf)
+
+c_ordc1_fnf
+
 
 # previous month
-p_month <- as.yearmon( (Sys.Date())) - 1/12 
-p_month <- p_month %>% format("%Y%m")
-p_month
 
-p_month_url <- paste0("http://cdec.water.ca.gov/reportapp/javareports?name=FNF.", p_month)
+# previous published month
+p_monyr <- as.yearmon(c_monyr) - 1/12
+p_mon <- month(p_monyr)
+p_mon
+p_yr <- year(p_monyr)
+p_yr
+p_monyr <- p_monyr %>% format("%Y%m")
+p_monyr
+#p_month_url <- paste0("http://cdec.water.ca.gov/reportapp/javareports?name=FNF.", p_monyr)
+p_month_url <- "http://cdec.water.ca.gov/reportapp/javareports?name=FNF.201906"
 p_month_url
 
 p_fnf <-  readHTMLTable(p_month_url, as.data.frame = TRUE)
 
-p_fnf_toptable <- c_fnf[[1]]
-#p_fnf_bottable <- c_fnf[[2]]  #only need top table for oroville
+p_fnf_toptable <- p_fnf[[1]]
+#p_fnf_bottable <- p_fnf[[2]]  #only need top table for oroville
+
+p_ordc1_fnf <- p_fnf_toptable %>% transmute(V1, V5)
+p_ordc1_fnf <- p_ordc1_fnf[-1,] %>% transmute(day = V1, p_ordc1_fnf = V5)
+p_ordc1_fnf <- p_ordc1_fnf %>% mutate(day = as.numeric(as.character(day)))
+p_ordc1_fnf <- na.omit(p_ordc1_fnf)
+p_ordc1_fnf$p_ordc1_fnf <- gsub(",", "", p_ordc1_fnf$p_ordc1_fnf)
+p_ordc1_fnf <- p_ordc1_fnf %>% transmute(day, ordc1_fnf = as.numeric(as.character(p_ordc1_fnf)))
+p_ordc1_fnf <- p_ordc1_fnf %>% transmute(chps_date = paste0(p_yr,"-", p_mon, "-", day), 
+                                         chps_date = ymd(chps_date) + 1, ordc1_fnf)
+p_ordc1_fnf
+
+
+
+orov_fnf <- rbind(c_ordc1_fnf, if(exists("p_ordc1_fnf")) p_ordc1_fnf)
+orov_fnf
+rm(list=setdiff(ls(), keepers))
+
+
+  
